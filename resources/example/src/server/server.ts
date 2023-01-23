8d124846020d10b593ee9ab36e1198bcb9d49233
Natan <natanfernandes@protonmail.com>
2023-01-23 02:44:46 -0300

    prisma is not needed on example

--- resources/example/src/server/server.ts
+++ resources/example/src/server/server.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import { onNetPromise } from '@common/shared';
 
-const prisma = new PrismaClient(); 
+// const prisma = new PrismaClient(); 
 
 onNetPromise<string>('test', (source, message) => {
   const response: string = `This was called by ${source} with the following message: ${message}`
